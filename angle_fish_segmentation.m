d1773611de0ee9c882e718070190e4968204400a
LSD <625331020@qq.com>
2019-04-16 16:13:52 +0800

    Solve the angle discontinuous between 2nd and 3rd cut; Add angle and diagonal control factor

--- angle_fish_segmentation.m
+++ angle_fish_segmentation.m
@@ -3,15 +3,20 @@ clear all;
 points = csvread('scan_fish1.csv');
 [row, col] = size(points);
 
+mess_factor = 0.02;     %片质量调整因子，其值为质量的控制误差，取值越小，质量控制越精确
+angle_factor = 0.5;       %角调整因子，取值0到1，值越小角度变化越小，最大步进调整角度1°，取0时仅调整对角线
+dia_factor = 3;         %对角调整因子，取值0到1，值越小对角线变化越小，最大步进调整0.5mm，取0时进调整角度
+
 total_volume = 0;
 piece_volume = 100000; %设定切片体积为40000立方毫米
-err_allowed = piece_volume * 0.05; %单片切片允许误差
+err_allowed = piece_volume * mess_factor; %单片切片允许误差
+dia_err = 0.05;
 cut_begin = 100;        %定义起刀位置
-% cut_angle = pi / 4;     %切刀角度
+cut_angle = pi / 4;     %切刀角度
 
 x_resolution = 0.3;
 y_resolution = 0.5;
-z_scale = 100;
+z_scale = 100;  
 
 for i = 1:row
     for j = 1:col
@@ -40,18 +45,19 @@ for i = 1:row
 end
 disp(['经过滤波鱿鱼的总体积为：' num2str(fil_total_volume) '立方毫米']);
 
-
-% points = flipud(points);        %水平翻转令尾端在矩阵横向开头处，实际扫描时，应当把尾端朝向传送带前进方向，这步省略
-
-
-% figure;
-% mesh(points);
-% hold off
-% title('尺度变换后的图像，纵坐标以百分之一毫米为单位');
+%% 初始偏移
+for i = 1:row           %找到开始偏移
+    row_length = sum(points(i, :)~=0);
+    if row_length > 10
+        points = points(i:end, :);
+        break;
+    end
+end
+[row, col] = size(points);
 
 %% 获取鱿鱼特征信息
-%获取鱿鱼侧面轮廓
-points_profile = points(:, fix(col*0.1));
+%获取鱿鱼侧面轮廓，以鱿鱼最长处为侧面轮廓
+points_profile = points(:, sum(points) == max(sum(points)));
 profile_col = 1:row;
 figure;
 plot(profile_col, points_profile);
@@ -81,8 +87,9 @@ end
 % mesh(cal_points)
 
 min_cost = 10000;           %设定初始的最小代价，大一点
-for cut_angle = pi/6 : pi/180 : pi/4
+for begin_cut_angle = pi/6 : pi/90: pi/3
 %以能够大头方向最凹点为基准，确定第一刀的落刀位置
+    cut_angle = begin_cut_angle;
     for i = 1:row
         row_length = sum(cal_points(i, :) ~= 0);
         row_length_1cm = sum(cal_points(i + 20, :) ~= 0);
@@ -92,13 +99,8 @@ for cut_angle = pi/6 : pi/180 : pi/4
             break;
         end
     end
-
-% mesh(cut);
-%   
-
+    
 %% 切第一刀
-
-   
     first_cut_volume = cal_volume(cut, cut_start, cal_points, row, col, x_resolution, y_resolution, z_scale);
 %     disp(['第一刀废料体积为：' num2str(fil_total_volume-first_cut_volume-tail_volume) '立方毫米']);
 %     disp(['第一刀角度：' num2str(cut_angle/pi*180) '°']);
@@ -106,14 +108,13 @@ for cut_angle = pi/6 : pi/180 : pi/4
     %% 切刀开始初始化
     cuts = cut_start;                                  %用于存储切刀落刀位置
     cut_angles = cut_angle;                            %存储落刀角度
-    cut_gap = 60;
+    cut_volumes = [];                                  %存储切片体积
+    cut_gap = 0;
     logical cut_finish;
     cut_finish = 0;
     first_cut_start = cut_start;
-    dia_err = 0.05;
 
-
-    %%  首先根据第一刀确定对角长度
+    %%  首先根据前两刀确定对角长度
     next_cut_start = first_cut_start;
     while(1)
         next_cut = generate_cut(next_cut_start, cut_angle, row, col, y_resolution, z_scale);                   
@@ -121,19 +122,18 @@ for cut_angle = pi/6 : pi/180 : pi/4
         if next_cut_volume < (piece_volume + err_allowed) 
             cut_finish = 1;
         end
-        if (first_cut_volume - next_cut_volume) < (piece_volume - err_allowed) 
-            next_cut_start = next_cut_start + 1;
-        elseif (piece_volume + err_allowed) < (first_cut_volume - next_cut_volume)
-            next_cut_start = next_cut_start - 1;            
+        if (first_cut_volume - next_cut_volume) < piece_volume
+            next_cut_start = next_cut_start + 1;        
         else                        %此时已经找到切刀位置
 %             disp(['切片体积：' num2str(first_cut_volume - next_cut_volume) '立方毫米']);
-            [intersection, set_dia_length] = cal_dia(cuts(end), next_cut_start, cut_angle, y_resolution, z_scale, points_profile, row);     %根据第一刀确定切片对角长度
+            cut_volumes = first_cut_volume - next_cut_volume;
+            [intersection, dia_length] = cal_dia(cuts(end), next_cut_start, cut_angle, y_resolution, z_scale, points_profile, row);     %根据第一刀确定切片对角长度
+            set_dia_length = dia_length / (1 + dia_err);                    %由于鱿鱼第一片高度低于后几片，因此第一刀的对角线应当设定对角线的上限
 %             disp(['此片对角线长度（设定对角长度）' num2str(set_dia_length) '毫米']);
 %             disp(['第二刀角度：' num2str(cut_angle/pi*180) '°']);
             break;
         end
     end
-    cut_gap = next_cut_start - first_cut_start;
     cuts = [cuts next_cut_start];
     cut_angles = [cut_angles cut_angle];
 
@@ -144,9 +144,9 @@ for cut_angle = pi/6 : pi/180 : pi/4
 % 
     while(1)   
         next_cut_start = pre_cut_start + cut_gap;          %预判下一刀的落刀位置，初始认为是60，此后依次以最新情况迭代
+      
         %查找固定对角线的下刀位置
         while(1)
-
             [intersection, dia_len] = cal_dia(cuts(end), next_cut_start, cut_angle, y_resolution, z_scale, points_profile, row);
             if dia_len < set_dia_length * (1 - dia_err)
                 next_cut_start = next_cut_start + 1;
@@ -165,25 +165,30 @@ for cut_angle = pi/6 : pi/180 : pi/4
                 cut_finish = 1;
             end
             if (pre_cut_volume - next_cut_volume) < (piece_volume - err_allowed) 
-                cut_angle = cut_angle + pi/180;
+                cut_angle = cut_angle + pi/180 * angle_factor;
+                intersection = intersection + dia_factor;
                 if cut_angle >= pi/2
 %                     disp(['切片体积：' num2str(pre_cut_volume - next_cut_volume) '立方毫米']);
+                    cut_volumes = [cut_volumes (pre_cut_volume - next_cut_volume)];
                     [intersection, dia_length] = cal_dia(cuts(end), next_cut_start, cut_angle, y_resolution, z_scale, points_profile, row);
 %                     disp(['此片对角线长度' num2str(dia_length) '毫米']);
                     break;
                 end
-                next_cut_start = find_cut_byinter(intersection, cut_angle, points_profile, y_resolution, z_scale);
+                next_cut_start = find_cut_byinter(fix(intersection), cut_angle, points_profile, y_resolution, z_scale);
             elseif (piece_volume + err_allowed) < (pre_cut_volume - next_cut_volume)
-                cut_angle = cut_angle - pi/180;
+                cut_angle = cut_angle - pi/180 * angle_factor;
+                intersection = intersection - pi/180 * dia_factor;
                 if cut_angle <= pi/12
 %                     disp(['切片体积：' num2str(pre_cut_volume - next_cut_volume) '立方毫米']);
+                    cut_volumes = [cut_volumes (pre_cut_volume - next_cut_volume)];
                     [interseciton, dia_length] = cal_dia(cuts(end), next_cut_start, cut_angle, y_resolution, z_scale, points_profile, row);
 %                     disp(['此片对角线长度' num2str(dia_length) '毫米']);
                     break;
                 end
-                next_cut_start = find_cut_byinter(intersection, cut_angle, points_profile, y_resolution, z_scale);          
+                next_cut_start = find_cut_byinter(fix(intersection), cut_angle, points_profile, y_resolution, z_scale);          
             else                        %此时已经找到切刀位置
 %                 disp(['切片体积：' num2str(pre_cut_volume - next_cut_volume) '立方毫米']);
+                cut_volumes = [cut_volumes (pre_cut_volume - next_cut_volume)];
                 [intersection, dia_length] = cal_dia(cuts(end), next_cut_start, cut_angle, y_resolution, z_scale, points_profile, row);
 %                 disp(['此片对角线长度' num2str(dia_length) '毫米']);
 %                 disp(['切片角度：' num2str(cut_angle/pi*180) '°']);
@@ -205,12 +210,18 @@ for cut_angle = pi/6 : pi/180 : pi/4
         pre_cut_volume = next_cut_volume;
     end
     
-    cut_divergence = cut_angles - [cut_angles(2:end) cut_angles(1)];
-    cost = sum(cut_divergence(2:end).^2);    %两刀差的二次代价函数
+%根据角度变化情况选择代价函数
+%     cut_divergence = cut_angles - [cut_angles(2:end) cut_angles(1)];
+%     cost = sum(cut_divergence(1:end-1).^2);    %两刀差的二次代价函数
+
+%根据切割均值情况选择代价函数
+    cost = abs(mean(cut_volumes) - piece_volume);
+    
     if cost < min_cost 
         min_cost = cost;
         final_cuts = cuts;
         final_cut_angles = cut_angles;
+        final_cut_volumes = cut_volumes;
     end
 end
 
@@ -231,5 +242,4 @@ for cut_index = 1:length(final_cuts)
     mesh(cut);
 end
      
-            
         
